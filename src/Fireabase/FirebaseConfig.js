9ddb55bf2b6a3616e9d951747cad370bf40f78c0
utshobarua10 <utshobarua10@gmail.com>
2023-06-22 13:29:55 +0600

    added all env file

--- src/Fireabase/FirebaseConfig.js
+++ src/Fireabase/FirebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC61JjE1cyHaSacZE9MKM4IY2CvsEcdnBY",
-  authDomain: "toy-city-851ff.firebaseapp.com",
-  projectId: "toy-city-851ff",
-  storageBucket: "toy-city-851ff.appspot.com",
-  messagingSenderId: "157440220008",
-  appId: "1:157440220008:web:10f91e57de165f637014dd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
